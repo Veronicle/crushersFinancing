c7066f2d664119b02ee6a577459ae62bf4e0ebf4
Veronicle <araashahuja28@gmail.com>
2022-07-10 15:20:36 -0400

    Created item and seller classes

---
+++ items.py
@@ -0,0 +1,19 @@
+class item:
+    
+    def __init__(self, style, color, logo, size, price):
+        self.style = style
+        self.color = color
+        self.logo = logo
+        self.size = size
+        self.price = price
+
+class seller:
+
+    def __init__(self, name, salesCount, salesValue, retailValue, profit):
+        self.name = name
+        self.salesCount = salesCount
+        self.retailValue = retailValue
+        self.salesValue = salesValue 
+        self.profit = profit
+
+
